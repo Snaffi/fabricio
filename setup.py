a5f1c1d29a82dcdfd63a53ded5898cb873be5de0
Rinat Khabibiev <srenskiy@gmail.com>
2017-03-19 17:41:09 +0300

    improved versions detection

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import importlib
+import re
 import sys
 
 from distutils.core import setup
@@ -13,13 +14,14 @@ required_six_version = (1,4,0)
 required_setuptools_version = (18,5)
 required_pyparsing_version = (2,0,1)
 
-install_requires = [
-    'Fabric>=1.1,<2.0',
-    'frozendict>=1.2,<2.0',
-    'cached-property>=1.3',
-    'docker-py>=1.8.1,<2.0',
-    'dpath>=1.4.0',
-]
+install_requires = {
+    'Fabric': '>=1.1,<2.0',
+    'frozendict': '>=1.2,<2.0',
+    'cached-property': '>=1.3',
+    'docker-py': '>=1.8.1,<2.0',
+    'dpath': '>=1.4.0',
+    'six': '>=1.4.0',
+}
 
 min_fixed_versions = {
     'six': (1,4,0),
@@ -30,25 +32,21 @@ min_fixed_versions = {
 for module_name, min_version in min_fixed_versions.items():
     try:
         module = importlib.import_module(module_name)
-        installed_version = tuple(map(int, module.__version__.split('.')))
+        version_found = re.match(r'[\\.\d]+', getattr(module, '__version__', ''))
+        if not version_found:
+            continue
+        version = version_found.group(0)
+        installed_version = tuple(map(int, version.split('.')))
     except ImportError:
         installed_version = ()
 
     if installed_version >= min_version:
-        install_requires.append('{module}=={version}'.format(
-            module=module_name,
-            version='.'.join(map(str, installed_version)),
-        ))
+        install_requires[module] = '==' + module.__version__
     else:
-        install_requires.append('{module}>={version}'.format(
-            module=module_name,
-            version='.'.join(map(str, min_version)),
-        ))
+        install_requires[module] = '>=' + '.'.join(map(str, min_version))
 
 if sys.version_info < (2,7):
-    install_requires.append(
-        'ordereddict>=1.1',
-    )
+    install_requires['ordereddict'] = '>=1.1'
 
 setup(
     name='fabricio',
@@ -72,5 +70,5 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Operating System :: OS Independent',
     ],
-    install_requires=install_requires,
+    install_requires=map(''.join, install_requires.items()),
 )
